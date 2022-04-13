047d9939f4300c21d1552bfe3bc2dbfa0ac131bc
kykyka <e.korj87@gmail.com>
2022-04-13 16:28:05 +0300

    part 2

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 "use strict";
 
-//1:
-    fetch("https://jsonplaceholder.typicode.com/albums")
-        .then(response => response.json())
-        .then(albums => {
-            const list = document.querySelector('#albums')
-            albums.forEach(element => {
+fetch("https://jsonplaceholder.typicode.com/albums")
+    .then(response => response.json())
+    .then(albums => {
+        const list = document.querySelector('#albums')
+        albums.forEach(element => {
 
-                const title = document.createElement("li");
-                title.classList.add("album_title")
-                title.innerText = element.title
-                list.append(title)
-            //    console.log(element.title)
-           });
-            console.log(albums)
-        } )
-        .catch((error) => console.error("Error"))
+            const title = document.createElement("li");
+            title.classList.add("album_title")
+            title.innerText = element.title
+
+            const button = document.createElement("button");
+            button.classList.add("btn")
+            button.innerText = "Delete"
+
+            title.append(button)
+            list.append(title)
+
+            title.addEventListener("click", (event) => {
+                const isRemoveButton = event.target.className === "btn";
+
+                if(isRemoveButton){
+                    const item = event.target.closest(".album_title");
+                    //   console.log(item);
+                    item.remove();
+                }
+            });
+
+        });  console.log(albums)
+    } ) .catch((error) => console.error("Error"))
 
 
   
